ea0fe7d212b0597ff38aa8bd4c842bfeb6bad94a
bjohan <johan.o.bodin@gmail.com>
2024-04-03 21:45:06 +0200

    Use python3 instead of 3.6

--- filter_identification.py
+++ filter_identification.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.6
+#!/usr/bin/python3
 import skrf as rf
 from matplotlib import pyplot as plt
 import numpy as np
--- remove_noise.py
+++ remove_noise.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.6
+#!/usr/bin/python3
 
 import skrf as rf
 from skrf.calibration import TwelveTerm
